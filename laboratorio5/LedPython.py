ff199ca196726f98ba8dea50fee62eebe3c95f1c
PabloGutierrez2002 <88423734+PabloGutierrez2002@users.noreply.github.com>
2022-03-12 17:59:09 -0600

    Add files via upload

---
+++ laboratorio5/LedPython.py
@@ -0,0 +1,25 @@
+import pyfirmata
+import time
+board = pyfirmata.Arduino('COM3')
+while True:
+   board.digital[13].write(1)
+   board.digital[12].write(1)
+   time.sleep(.5)
+   board.digital[13].write(0)
+   board.digital[12].write(0)
+   time.sleep(.5)
+   board.digital[4].write(1)
+   board.digital[2].write(1)
+   time.sleep(.5)
+   board.digital[4].write(0)
+   board.digital[2].write(0)
+   time.sleep(.5)
+   board.digital[7].write(1)
+   time.sleep(.00001)
+   board.digital[7].write(0)
+   time.sleep(.00001)
+   board.digital[8].write(1)
+   time.sleep(.00001)
+   board.digital[8].write(0)
+   time.sleep(.00001)
+
